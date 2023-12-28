0bb83aca4164e41fae729f50d0e71c32c323bc0a
dhducpro <49698925+dhducpro@users.noreply.github.com>
2023-12-28 11:30:40 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -26,7 +26,7 @@ SOURCE_FOLDER="$PWD/_site"
 echo "Building website ($BUILD_ENV)..."
 JEKYLL_ENV=$BUILD_ENV bundle exec jekyll build
 
-echo "Deploying ($SOURCE_FOLDER) to ($USER@$HOST:$TARGET_FOLDER)..."
+echo "Deploying ($SOURCE_FOLDER) to (https://www.acml-conf.org/2024/)..."
 
 lftp -f "
 set ftp:passive-mode $PASSIVE_MODE
